c7712e32683a7eba74a414e4ce20403115bca221
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-06-18 08:52:34 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- * this program prints "Putchar all single digit numbers starting from 0
+ * this program prints "Print all single digit numbers starting from 0
  * Return: 0
  */
 int main(void)
