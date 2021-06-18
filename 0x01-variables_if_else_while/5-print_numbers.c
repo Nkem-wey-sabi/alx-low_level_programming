2320c92162b78e47d258afa01cda070ed0b84a6f
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-06-18 08:34:41 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Prints all single digit numbers starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n < 10 ; n++)
+{
+	printf(n);
+}
+	printf('\n');
+	return (0);
+}
+
