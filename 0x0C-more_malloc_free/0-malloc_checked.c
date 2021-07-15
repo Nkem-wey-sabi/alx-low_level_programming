d957ef5ed4c55b799372cffa56357a375e727485
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-07-15 11:23:28 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,8 @@
 
 void *malloc_checked(unsigned int b)
 {
-  void *ptr;
-
-  ptr = malloc(b);
-  if (ptr == NULL)
-    exit(98);
-  return (ptr);
+void *mem = malloc(b);
+	if (!mem)
+		exit(98);
+	return (mem);
 }
