d7abe94ba2eeda0801c1c47a49485fac340f11d9
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-07-19 16:35:59 -0700

    1st project of the day

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,8 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
-
+/*
+ * Header file that defines a macro named size
+ */
 #define SIZE 1024
-typedef int make_iso_compilers_happy;
 
 #endif 
