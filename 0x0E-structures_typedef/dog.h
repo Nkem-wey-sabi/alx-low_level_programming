944bd1ffca867d049708f2416ed3eed5e8316ae7
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-07-19 08:40:42 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,11 @@
  * @age: dog age
  * @owner: owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+};
 
 #endif
