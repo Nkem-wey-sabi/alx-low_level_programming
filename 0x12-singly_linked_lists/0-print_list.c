59286937af38f0c041ee9b63dce35d6fdaac2a38
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-07-29 08:16:31 -0700

    corrected print_list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,14 @@ size_t print_list(const list_t *h)
 	n = 0;
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		n++;
+		h = h->next;
 	}
 	return (n);
 }
