7256551b252dcbc6f82b0f7ab651388286ebfb44
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-08-09 12:25:29 -0700

    added read function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,45 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to POSIX standard output.
- * @filename: file to be read.
- * @letters: bytes to read and print.
- * Return: ssize_t, bytes read.
- *          0, if error.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n;
+	ssize_t lenr, lenw;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
-	n = read(fd, buffer, letters);
-	if (n == -1)
+	if (buffer == NULL)
 	{
 		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
 		free(buffer);
 		return (0);
 	}
-	write(STDOUT_FILENO, buffer, n);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	close(fd);
-	return (n);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
